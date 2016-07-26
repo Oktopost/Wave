761a979c1dc4f33a819809270887ea1ea8563e7d
Alexey Puchkov <alexey@oktopost.com>
2016-07-26 13:26:17 +0300

    Add package object

---
+++ src/Wave/Objects/Package.php
@@ -0,0 +1,53 @@
+<?php
+namespace Wave\Objects;
+
+
+use Objection\LiteSetup;
+use Objection\LiteObject;
+
+
+/**
+ * @property string $Name
+ * @property string $Version
+ * @property string $BuildTarget
+ * @property int $Timestamp
+ */
+class Package extends LiteObject
+{
+	/**
+	 * @return array
+	 */
+	protected function _setup()
+	{
+		return [
+			'Name'			=> LiteSetup::createString(),
+			'Version'		=> LiteSetup::createString(),
+			'BuildTarget'	=> LiteSetup::createString(),
+			'Timestamp'		=> LiteSetup::createInt()
+		];
+	}
+	
+	
+	public function touch()
+	{
+		$this->Timestamp = time();
+	}
+	
+	
+	/**
+	 * @param string $version
+	 * @param string $buildTarget
+	 * @return Package
+	 */
+	public static function create($version, $buildTarget)
+	{
+		$package = new Package();
+		
+		$package->Name			= "$version-$buildTarget";
+		$package->Version		= $version;
+		$package->BuildTarget	= $buildTarget;
+		$package->Timestamp		= time();
+		
+		return $package;
+	}
+}
---
+++ tests/Wave/Objects/PackageTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace Wave\Objects;
+
+
+class PackageTest extends \PHPUnit_Framework_TestCase
+{
+	public function test_touch_TimestampUpdated()
+	{
+		$p = new Package();
+		
+		$p->Timestamp = 12;
+		$p->touch();
+		
+		$this->assertEquals(time(), $p->Timestamp, '', 1);
+	}
+	
+	
+	public function test_create_PackageReturned()
+	{
+		$this->assertInstanceOf(Package::class, Package::create('a', 'b'));
+	}
+	
+	public function test_create_VersionSet()
+	{
+		$this->assertEquals('a', Package::create('a', 'b')->Version);
+	}
+	
+	public function test_create_BuildSet()
+	{
+		$this->assertEquals('b', Package::create('a', 'b')->BuildTarget);
+	}
+	
+	public function test_create_TimestampSet()
+	{
+		$this->assertEquals(time(), Package::create('a', 'b')->Timestamp, '', 1);
+	}
+	
+	public function test_create_NameSet()
+	{
+		$this->assertEquals('a-b', Package::create('a', 'b')->Name);
+	}
+}
