f3582d80085e95af11d966d8c9f1be8d971b159a
Alexey Puchkov <alexey@oktopost.com>
2016-07-31 17:46:42 +0300

    Add unit test for SourceConnectorFactory

---
+++ tests/Wave/Source/SourceConnectorFactoryTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace Wave\Source;
+
+
+use Wave\Config;
+use Wave\Enum\SourceType;
+use Wave\Scope;
+use Wave\Source\Git\GitSource;
+
+
+class SourceConnectorFactoryTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @expectedException \Wave\Exceptions\WaveException
+	 */
+	public function test_getConnector_InvalidType_ErrorThrown()
+	{
+		$f = new SourceConnectorFactory();
+		$f->getConnector('not_existing');
+	}
+	
+	/**
+	 * @expectedException \Wave\Exceptions\WaveException
+	 */
+	public function test_getConnector_SvnConnector_ErrorThrown()
+	{
+		$f = new SourceConnectorFactory();
+		$f->getConnector(SourceType::SVN);
+	}
+	
+	public function test_getConnector_GitConnector_GitSourceInstanceReturned()
+	{
+		/** @var Config $config */
+		$config = $this->getMock(Config::class, [], [], '', false);
+		Scope::instance()->setConfig($config);
+		
+		$f = new SourceConnectorFactory();
+		$this->assertInstanceOf(GitSource::class, $f->getConnector(SourceType::GIT));
+	}
+}
